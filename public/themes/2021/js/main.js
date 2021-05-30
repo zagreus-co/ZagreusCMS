2ff09931811abcbb8a593d8cda85fbbaa95a870c
unknown <white_joker20@yahoo.com>
2021-05-30 18:34:58 +0430

    Improve reply mechanism

--- public/themes/2021/js/main.js
+++ public/themes/2021/js/main.js
@@ -10,4 +10,12 @@ let toggleHiddenElement = (element, toggleClass = 'block') => {
 let replyComment = (comment_id) => {
     document.querySelector('#commentReplyForm').scrollIntoView({behavior: 'smooth'});
     document.querySelector('#commentReplyForm input[name=parent_id]').value = comment_id
+    document.querySelector('#commentReplyForm div#replyTo').classList.remove('hidden');
+    document
+        .querySelector('#commentReplyForm div#replyTo p').innerHTML = document.querySelector(`#comment_${comment_id}`).innerHTML;
+}
+
+let closeReply = () => {
+    document.querySelector('#commentReplyForm input[name=parent_id]').value = 0;
+    document.querySelector('#commentReplyForm div#replyTo').classList.add("hidden");
 }
