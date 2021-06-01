84f844e2f8f0c6e012e461f0846ec954730403c0
unknown <white_joker20@yahoo.com>
2021-06-02 00:13:14 +0430

    Create Analytics module

---
+++ Modules/Analytics/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('analytics::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('analytics.name') !!}
+    </p>
+@endsection
