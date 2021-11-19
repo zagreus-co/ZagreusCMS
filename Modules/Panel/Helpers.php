0d3e59732b22c2344af0fb1d949ae989b5df159c
mehrab-wj <white_joker20@yahoo.com>
2021-11-19 23:23:45 +0330

    move panel helpers to codebase

--- Modules/Panel/Helpers.php
+++ Modules/Panel/Helpers.php
@@ -1,20 +1,2 @@
 <?php
-use Illuminate\Support\Facades\Route;
 
-if(! function_exists('isActive') ) {
-    
-    function isActive($key , $activeClassName = 'active', $default = '') {
-
-        if(is_array($key)) {
-            return in_array(Route::currentRouteName() , $key) ? $activeClassName : $default;
-        }
-
-        // check for naming patterns like: module.moduleName.*
-        if(!is_array($key) && strpos($key, '*') !== false) {
-            return (strpos(Route::currentRouteName(), str_replace('*', '', $key)) !== false) ? $activeClassName :  $default;
-        }
-
-        return Route::currentRouteName() == $key ? $activeClassName :  $default;
-    }
-
-}
--- app/Helpers.php
+++ app/Helpers.php
@@ -1,9 +1,24 @@
 <?php
+use Illuminate\Support\Facades\Route;
 
-if ( !function_exists('locales') ) {
-    function locales() { return config('app.available_locales'); }
+// Global Helpers
+if ( !function_exists('locales') ) { function locales() { return config('app.available_locales'); } }
+
+// Panel Helpers
+if(! function_exists('isActive') ) {
+    function isActive($key , $activeClassName = 'active', $default = '') {
+        if(is_array($key))
+            return in_array(Route::currentRouteName() , $key) ? $activeClassName : $default;
+
+        // check for naming patterns like: module.moduleName.*
+        if(!is_array($key) && strpos($key, '*') !== false)
+            return (strpos(Route::currentRouteName(), str_replace('*', '', $key)) !== false) ? $activeClassName :  $default;
+
+        return Route::currentRouteName() == $key ? $activeClassName :  $default;
+    }
 }
 
+// Panel-Theme helpers
 if (!function_exists('viewIfExist')) {
     function viewIfExist($view_name, $variables = [], $error_view = 'errors.theme404') {
         if ( view()->exists($view_name) ) {
