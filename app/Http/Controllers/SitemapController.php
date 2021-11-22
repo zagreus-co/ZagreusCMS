16608edf5380481f485c81c9250f3f5b6e7bfe74
mehrab-wj <white_joker20@yahoo.com>
2021-11-22 23:55:43 +0330

    clearn old codes

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -11,10 +11,6 @@ class SitemapController extends Controller
         $sitemap = \App::make('sitemap');        
 
         $_sitemap = \Hooks::filter('sitemap.index', $sitemap);
-        // $sitemap->addSitemap( route('module.seo.sitemap.pages'), 
-        //     !is_null($this->_page) ? $this->_page::latest()->first()->created_at : null
-        // );
-        // $sitemap->addSitemap(route('module.seo.sitemap.categories'));
 
         return $sitemap->render('sitemapindex');
     }
