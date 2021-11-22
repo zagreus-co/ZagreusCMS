18ec391cbd9bf4716dc2a08dd149202b6863a957
mehrab-wj <white_joker20@yahoo.com>
2021-11-22 18:55:23 +0330

    update keyword module model

--- Modules/Keyword/Entities/Keyword.php
+++ Modules/Keyword/Entities/Keyword.php
@@ -9,7 +9,7 @@ class Keyword extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['keyword'];
+    protected $fillable = ['keyword', 'keywordable_id', 'keywordable_type'];
     public $timestamps = false;
 
     protected static function newFactory()
@@ -19,5 +19,17 @@ class Keyword extends Model
 
     public function keywordable() {
         return $this->morphTo();
+    }
+
+    public function createKeyword(array $keywords, $keywordable_id, $keywordable_type) {
+        $this->where('keywordable_id', $keywordable_id)
+            ->where('keywordable_type', $keywordable_type)->delete();
+        foreach ($keywords as $keyword) {
+            $this->create([
+                'keyword'=> $keyword,
+                'keywordable_id'=> $keywordable_id,
+                'keywordable_type'=> $keywordable_type,
+            ]);
+        }
     }
 }
