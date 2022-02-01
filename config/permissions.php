5dcf60f49d10d73283f5d9ee490d84afa3134e3a
mehrab-wj <white_joker20@yahoo.com>
2022-02-01 20:00:40 +0330

    Add core permissions to seeder command

--- config/permissions.php
+++ config/permissions.php
@@ -2,4 +2,8 @@
 
 return [
     "manage_themes"=> "Add, Edit, Delete themes",
+    "manage_roles"=> "Add, Edit, Delete roles",
+    "manage_options"=> "Add, Edit, Delete options",
+    "manage_users"=> "Add, Edit, Delete users",
+    "manage_media"=> "Add, Edit, Delete media",
 ];
