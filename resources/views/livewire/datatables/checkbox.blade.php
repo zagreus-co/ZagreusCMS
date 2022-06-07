c3cdfbc3f5679f485ccc051714f9b96bbf46d130
mehrab-wj <white_joker20@yahoo.com>
2022-06-07 20:05:43 +0430

    WIP: fix: livewire datatable styles

---
+++ resources/views/livewire/datatables/checkbox.blade.php
@@ -0,0 +1,9 @@
+<div class="flex justify-center">
+    <input
+        type="checkbox"
+        wire:model="selected"
+        value="{{ $value }}"
+        @if (property_exists($this, 'pinnedRecords') && in_array($value, $this->pinnedRecords)) checked @endif
+        class="w-4 h-4 mt-1 text-blue-600 form-checkbox transition duration-150 ease-in-out"
+    />
+</div>
