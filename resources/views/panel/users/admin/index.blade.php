2c69ca4e1e342be1f9404010c7283619899364d0
mehrab-wj <white_joker20@yahoo.com>
2022-06-07 19:50:40 +0430

    WIP: fix: users report card

--- resources/views/panel/users/admin/index.blade.php
+++ resources/views/panel/users/admin/index.blade.php
@@ -1,65 +1,55 @@
 @extends(panelLayout())
 
 @section('content')
-<div class="grid grid-cols-4 gap-6 mt-6 xl:grid-cols-2">
-    <div class="report-card">
-        <div class="card">
-            <div class="card-body flex flex-col">
-                <div class="flex flex-row justify-between items-center">
-                    <div>
-                        <h1 class="h5">{{ number_format(\App\Models\User::count()) }}</h1>
-                        <p>{{ __('Total users') }}</p>
-                    </div>
-                    <div class="h6 text-indigo-700 fad fa-users"></div>
+<div class="grid xl:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-6 mt-6">
+    <div class="card hover:shadow bg-white hover:bg-indigo-50 transition duration-200">
+        <div class="card-body flex flex-col">
+            <div class="flex flex-row justify-between items-center">
+                <div>
+                    <h3>{{ number_format(\App\Models\User::count()) }}</h3>
+                    <p>{{ __('Total users') }}</p>
                 </div>
+                <div class="text-indigo-700"><ion-icon name="people"></ion-icon></div>
             </div>
         </div>
-        <div class="footer bg-white p-1 mx-4 border border-t-0 rounded rounded-t-none"></div>
     </div>
-    
-    <div class="report-card">
-        <div class="card">
-            <div class="card-body flex flex-col">
-                <div class="flex flex-row justify-between items-center">
-                    <div>
-                        <h1 class="h5">{{ number_format(\App\Models\User::where('role_id', '!=', 0)->count()) }}</h1>
-                        <p>{{ __('Staff users') }}</p>
-                    </div>
-                    <div class="h6 text-green-600 fad fa-user-tie"></div>
+
+    <div class="card hover:shadow bg-white hover:bg-blue-50 transition duration-200">
+        <div class="card-body flex flex-col">
+            <div class="flex flex-row justify-between items-center">
+                <div>
+                    <h3>{{ number_format(\App\Models\User::where('role_id', '!=', 0)->count()) }}</h3>
+                    <p>{{ __('Staff users') }}</p>
                 </div>
+                <div class="text-blue-400"><ion-icon name="shield-half"></ion-icon></div>
             </div>
         </div>
-        <div class="footer bg-white p-1 mx-4 border border-t-0 rounded rounded-t-none"></div>
+    </div>
+
+    <div class="card hover:shadow bg-white hover:bg-gray-50 transition duration-200">
+        <div class="card-body flex flex-col">
+            <div class="flex flex-row justify-between items-center">
+                <div>
+                    <h3>{{ number_format(\App\Models\User::where('role_id', 0)->count()) }}</h3>
+                    <p>{{ __('Normal users') }}</p>
+                </div>
+                <div class="text-gray-500"><ion-icon name="walk"></ion-icon></div>
+            </div>
+        </div>
     </div>
 
     <div class="report-card">
-        <div class="card">
+        <div class="card hover:shadow bg-white hover:bg-yellow-50 transition duration-200">
             <div class="card-body flex flex-col">
                 <div class="flex flex-row justify-between items-center">
                     <div>
-                        <h1 class="h5">{{ number_format(\App\Models\User::where('role_id', 0)->count()) }}</h1>
-                        <p>{{ __('Normal users') }}</p>
-                    </div>
-                    <div class="h6 text-blue-600 fad fa-user"></div>
-                </div>
-            </div>
-        </div>
-        <div class="footer bg-white p-1 mx-4 border border-t-0 rounded rounded-t-none"></div>
-    </div>
-
-    <div class="report-card">
-        <div class="card">
-            <div class="card-body flex flex-col">
-                <div class="flex flex-row justify-between items-center">
-                    <div>
-                        <h1 class="h5">{{ number_format(\App\Models\User::whereDate('created_at', \Carbon\Carbon::today())->count()) }}</h1>
+                        <h3>{{ number_format(\App\Models\User::whereDate('created_at', \Carbon\Carbon::today())->count()) }}</h3>
                         <p>{{ __('Today registration') }}</p>
                     </div>
-                    <div class="h6 text-yellow-700 fad fa-user-plus"></div>
+                    <div class="text-yellow-500"> <ion-icon name="person-add"></ion-icon> </div>
                 </div>
             </div>
         </div>
-        <div class="footer bg-white p-1 mx-4 border border-t-0 rounded rounded-t-none"></div>
     </div>
 
 </div>
