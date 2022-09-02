02b4dd6216cadf0dfe33270ed994c7f8032944a7
mehrab-wj <white_joker20@yahoo.com>
2022-09-02 14:20:14 +0430

    feat: add function to sanitize and reduce content

--- Modules/Blog/Helpers.php
+++ Modules/Blog/Helpers.php
@@ -6,3 +6,10 @@ if (! function_exists("blogPosts") ) {
             ->latestPosts($limit);
     }
 }
+
+if (! function_exists("sanitizeContent") ) {
+    function sanitizeContent($content, $limit = 160) {
+        $replace_list = ["&nbsp;"=> " ", "&lt;"=> '<', '&gt;'=> '>', '&amp;'=> '&', '&euro;'=> '€', '&pound;'=> '£', '&quot;'=> '“', '&apos;'=> '‘'];
+        return str_replace(array_keys($replace_list), $replace_list, mb_substr( strip_tags($content), 0, $limit) );
+    }
+}
