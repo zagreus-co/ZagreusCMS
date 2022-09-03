e4bf1e6bfa0f5d2898f49fc806011e056408fdcd
mehrab-wj <white_joker20@yahoo.com>
2022-09-03 14:14:27 +0430

    WIP: complete pages template

---
+++ resources/views/themes/2022/page.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+@themeInclude('partials.head')
+<body>
+    @themeInclude('partials.header')
+    <article class='px-20 mt-4'>
+        <section class="flex items-center justify-between">
+            <small class="text-gray-500">
+                {{ $page->created_at->format('F j, Y') }}
+                <span> - {{ round(str_word_count(strip_tags($page->content)) / 165) }} min read</span>
+            </small>
+
+            <a href="{{ route('index') }}" class="px-3 py-1 hover:text-fuchsia-500 transition duration-200">Back to Home</a>
+        </section>
+
+        <header class="mt-1">
+            <h1 class='text-5xl'>{{ $page->title }}</h1>
+            @if ($page->cover)
+                <img class='w-full rounded-md mt-2' src="{{ $page->cover }}" alt="{{ $page->title }}">
+            @endif
+        </header>
+
+        <p class="mt-6">{!! $page->content !!}</p>
+
+    </article>
+    @themeInclude('partials.footer')
+</body>
+</html>
