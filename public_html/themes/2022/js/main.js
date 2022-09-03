071902d6d6e0bff161731f5099cefbefeb31a6ac
mehrab-wj <white_joker20@yahoo.com>
2022-09-03 19:52:47 +0430

    fix: check function initialized or not

--- public_html/themes/2022/js/main.js
+++ public_html/themes/2022/js/main.js
@@ -1,21 +1,27 @@
-let toggleHiddenElement = (element, toggleClass = 'block') => {
-    element = document.querySelector(element)
-    if ( element.classList.contains('hidden') ) {
-        element.classList.replace('hidden', toggleClass);
-    } else {
-        element.classList.replace(toggleClass, 'hidden');
+if (typeof toggleHiddenElement === 'undefined') {
+    let toggleHiddenElement = (element, toggleClass = 'block') => {
+        element = document.querySelector(element)
+        if ( element.classList.contains('hidden') ) {
+            element.classList.replace('hidden', toggleClass);
+        } else {
+            element.classList.replace(toggleClass, 'hidden');
+        }
     }
 }
 
-let replyComment = (comment_id) => {
-    document.querySelector('#commentReplyForm').scrollIntoView({behavior: 'smooth'});
-    document.querySelector('#commentReplyForm input[name=parent_id]').value = comment_id
-    document.querySelector('#commentReplyForm div#replyTo').classList.remove('hidden');
-    document
-        .querySelector('#commentReplyForm div#replyTo p').innerHTML = document.querySelector(`#comment_${comment_id}`).innerHTML;
+if (typeof replyComment === 'undefined') {
+    let replyComment = (comment_id) => {
+        document.querySelector('#commentReplyForm').scrollIntoView({behavior: 'smooth'});
+        document.querySelector('#commentReplyForm input[name=parent_id]').value = comment_id
+        document.querySelector('#commentReplyForm div#replyTo').classList.remove('hidden');
+        document
+            .querySelector('#commentReplyForm div#replyTo p').innerHTML = document.querySelector(`#comment_${comment_id}`).innerHTML;
+    }
 }
 
-let closeReply = () => {
-    document.querySelector('#commentReplyForm input[name=parent_id]').value = 0;
-    document.querySelector('#commentReplyForm div#replyTo').classList.add("hidden");
+if (typeof closeReply === 'undefined') {
+    let closeReply = () => {
+        document.querySelector('#commentReplyForm input[name=parent_id]').value = 0;
+        document.querySelector('#commentReplyForm div#replyTo').classList.add("hidden");
+    }
 }
