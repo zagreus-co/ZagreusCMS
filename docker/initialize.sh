39882f55c500afa60286bf3b2df7f323dfc7c190
Mehrab <white_joker20@yahoo.com>
2023-03-31 14:53:19 +0330

    fix: change docker configs
    use php-fpm and nginx instead of Laravel octane and swoole

--- docker/initialize.sh
+++ docker/initialize.sh
@@ -1,24 +1,31 @@
-#!/usr/bin/bash
+#!/bin/bash
+
+echo '[+] Sleep for 20 seconds to make sure database is booted...'
+sleep 20s
 
 CONTAINER_START_PATH="/CONTAINER_START_INFO"
+cd /app
+
 if [ -f "$CONTAINER_START_PATH" ]; then
-    echo "[+] Starting the swoole server..."
-    php artisan octane:start --host=0.0.0.0 --port=80
+    echo "[+] Starting the PHP-FPM..."
+    # php artisan octane:start --host=0.0.0.0 --port=8000
+
+    supervisord -c /etc/supervisor/conf.d/supervisord.conf
+
+    php-fpm -R
 else
     echo "Container first initialized at $(date) by $(whoami)" > $CONTAINER_START_PATH
     echo "[+] Initializing container at $(date)"
     
-    cp .env.example .env
-
-    # Installing PHP Dependencies
-    composer install
-    
-    # Generate application key
-    php artisan key:generate
+    echo '[+] Start installing migrations...'
 
     # Load migrations and seed database
     php artisan migrate --seed
     php artisan module:seed
     php artisan zagreus:load-permissions
-    php artisan octane:start --host=0.0.0.0 --port=80
+    # php artisan octane:start --host=0.0.0.0 --port=8000
+
+    supervisord -c /etc/supervisor/conf.d/supervisord.conf
+
+    php-fpm -R
 fi
