00b8f4aecbdd9132081a17e23c4ab689e47fad25
Mehrab <white_joker20@yahoo.com>
2023-04-10 23:13:58 +0330

    feat: make measurement middleware role based

--- app/Http/Middleware/PerformanceMeasurement.php
+++ app/Http/Middleware/PerformanceMeasurement.php
@@ -10,6 +10,15 @@ use Throwable;
 
 class PerformanceMeasurement
 {
+
+    /**
+     * When you set roles, performance measurement will only be available to users with specific role
+     * set empty array to make it available for all
+     *
+     * @var array
+     */
+    public array $roles = ['sudo'];
+
     /**
      * Handle an incoming request.
      *
@@ -19,6 +28,18 @@ class PerformanceMeasurement
     {
         // Check if performance measurement is enabled:
         if (!env('APP_DEBUG', false)) return $next($request);
+
+        // Check for roles based measurement
+        if (
+            count($this->roles) >= 0
+            && (!auth()->check())
+            || count($this->roles) >= 0
+            && (auth()->check()
+                && isset(auth()->user()->role)
+                && !in_array(auth()->user()->role->title, $this->roles))
+        ) {
+            return $next($request);
+        }
 
         try {
             /** @var \Illuminate\Http\Response $response */
