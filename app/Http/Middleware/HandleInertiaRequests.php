b22ee128985a6336b2c8eb24b5e4d43287115c3f
Mehrab <white_joker20@yahoo.com>
2024-01-16 19:43:22 +0330

    fix: send permissions to Inertia components

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -33,6 +33,7 @@ class HandleInertiaRequests extends Middleware
             ...parent::share($request),
             'auth' => [
                 'user' => $request->user(),
+                'permissions'=> app('userPermissions')
             ],
         ];
     }
